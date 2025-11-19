a7d726fe017d31da75975f9caa994d6b06a69e7e
AnnaPaytakov <paytakov.annamurad2003@gmail.com>
2025-11-19 17:42:26 +0500

    Update urls.py

--- videos/urls.py
+++ videos/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from . import views
+from .views import (
+    VideosListView,
+    UploadVideoView,
+    VideoDetailView,
+)
 
 urlpatterns = [
-    path("", views.videos, name="videos"),
-    path("upload/", views.upload_video, name="upload_video"),
-    path("video/<str:pk>/", views.video, name="video"),
+    path("", VideosListView.as_view(), name="videos"),
+    path("upload/", UploadVideoView.as_view(), name="upload_video"),
+    path("video/<int:pk>/", VideoDetailView.as_view(), name="video"),
 ]
